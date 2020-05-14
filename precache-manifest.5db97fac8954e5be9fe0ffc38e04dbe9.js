e552c12a0d010a125a34c4502d2473977f22625a
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-05-14 13:25:53 +0300

    Updates

---
+++ precache-manifest.5db97fac8954e5be9fe0ffc38e04dbe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07079d8850dbc8c14e09217d79bec8fa",
+    "url": "/react_todo_hooks/index.html"
+  },
+  {
+    "revision": "7de9b643fad6b2d5be77",
+    "url": "/react_todo_hooks/static/css/main.a55fb368.chunk.css"
+  },
+  {
+    "revision": "148a106249364909eed9",
+    "url": "/react_todo_hooks/static/js/2.264f0b60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_hooks/static/js/2.264f0b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de9b643fad6b2d5be77",
+    "url": "/react_todo_hooks/static/js/main.8f9479f7.chunk.js"
+  },
+  {
+    "revision": "056f8e906e566bcfe6e4",
+    "url": "/react_todo_hooks/static/js/runtime-main.1f134824.js"
+  }
+]);
